78f220bf8b0f7aa909ed6d3d1bfe6a5ef8372d35
tr1me <156481481+tr1me@users.noreply.github.com>
2025-01-17 10:30:00 -0600

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -14,7 +14,7 @@ ART = os.path.join(PATH, 'resources', 'media')
 #        User Edit Variables                            #
 #########################################################
 ADDONTITLE = '[COLOR limegreen][B]Open[/B][/COLOR]Wizard'
-BUILDERNAME = 'OpenWizard'
+BUILDERNAME = 'ColossalTV'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
 BUILDFILE = 'http://'
